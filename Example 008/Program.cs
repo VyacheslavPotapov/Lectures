01714b14d3bb71be8584b0aa3ec2a69e3081f224
Vyacheslavclear <mpk_optima-v@mail.ru>
2022-06-30 23:15:08 +0300

    new fix

--- Example 008/Program.cs
+++ Example 008/Program.cs
@@ -9,14 +9,14 @@ int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
-int b2 = 23;
+int b2 = 54;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
 int max1 = Max(a1, b1, c1);
-int max1 = Max(a2, b2, c2);
-int max1 = Max(a3, b3, c3);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
 int max = Max(max1, max2, max3); 
 Console.WriteLine(max);
--- Example11/Program.cs
+++ Example11/Program.cs
@@ -1,48 +1,53 @@
-﻿void FillArray(int [] collection)
+﻿internal class Program
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    private static void Main(string[] args)
     {
-       collection[index] = new Random().Next(1, 10);
-       index++;
+        void FillArray(int[] collection)
+        {
+            int length = collection.Length;
+            int index = 0;
+            while (index < length)
+            {
+                collection[index] = new Random().Next(1, 10);
+                index++;
+            }
+        }
+
+
+        void PrintArray(int[] col)
+        {
+            int count = col.Length;
+            int position = 0;
+            while (position < count)
+            {
+                Console.WriteLine(col[position]);
+                position++;
+            }
+        }
+
+        int IndexOf(int[] collection, int find)
+        {
+            int count = collection.Length;
+            int index = 0;
+            int position = -1;
+            while (index < count)
+            {
+                if (collection[index] == find)
+                {
+                    position = index;
+                    break;
+                }
+                index++;
+            }
+            return position;
+        }
+        int[] array = new int[10];
+
+        FillArray(array);
+        PrintArray(array);
+        Console.WriteLine();
+
+        int pos = IndexOf(array, 4);
+        Console.WriteLine(pos);
     }
 }
-
-
-void PrintArray(int[] col)
-{
-    int count = col.length;
-    int position = 0;
-    while (position < count)
-    {
-       Console.WriteLine(col[position]);
-       position++;
-    }
-    return
-}
-
-int indexof(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
-    while (index < count)
-    {
-       if(collection[index] == find)
-       {
-           position = index;
-           break;
-       }
-       index++;
-    }
-    return position;
-}
-int[] array = new int[10];
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-
-Int pos = IndexOf(array, 4);
-Console.WriteLine(pos);
